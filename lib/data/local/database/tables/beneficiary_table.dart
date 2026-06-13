cccd19f3419af9fe1a1c580ce68a9d21631d5ce6
mdRafizz <mdraf.cse@gmail.com>
2026-06-13 20:29:30 +0600

    new

--- lib/data/local/database/tables/beneficiary_table.dart
+++
@@ -1,9 +0,0 @@
-import 'package:drift/drift.dart';
-
-class BeneficiaryTable extends Table {
-  IntColumn get id => integer().autoIncrement()();
-  TextColumn get name => text().withLength(min: 1, max: 100).unique()();
-  TextColumn get relationship => text().nullable()();
-  IntColumn get colorInt => integer().nullable()();
-  BoolColumn get isActive => boolean().withDefault(const Constant(true))();
-}
